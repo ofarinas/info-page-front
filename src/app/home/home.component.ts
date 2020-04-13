d0b809368a8e79a23709e8271f79dadf39f62478
Osvlado Fernandes <ofarinas12@gmail.com>
2020-04-13 19:32:28 +0200

    added a copple of projects

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,17 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
 
+  }
+
+  download() {
+    const doc = new jsPDF();
+    doc.text(20, 20, 'Hello world!');
+    doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+    doc.addPage();
+    doc.text(20, 20, 'Do you like that?');
+
+    // Save the PDF
+    doc.save('Test.pdf');
   }
 
   ngAfterViewInit(): void {
