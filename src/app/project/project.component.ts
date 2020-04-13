43bd4f8dba72aa8d1b88f692dce70522b72aeca4
Osvlado Fernandes <ofarinas12@gmail.com>
2020-04-13 13:30:53 +0200

    added a copple of projects

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-project',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
+  panelOpenState = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
